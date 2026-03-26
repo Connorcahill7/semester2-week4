d92db66bd1a634b069e936e0cf0a91a7aab028f8
Connorcahill7 <dxvv0752@leeds.ac.uk>
2026-03-26 19:39:20 +0000

    df\

--- worksheet/portfolio/makeString.c
+++ worksheet/portfolio/makeString.c
@@ -1,7 +1,6 @@
-
 /*
-Name:
-Student ID:
+Name: Kieran Cahill
+Student ID: 201944641
 */
 
 #include <stdio.h>
@@ -10,8 +9,18 @@ Student ID:
 int main( int argc, char **argv ) {
 
     // define appropriate data to hold your answer
+    char buffer[100] = "";   // should be enough for max inputs
 
     // process the command-line data using appropriate string functions
+    for (int i = 1; i < argc; i++) {
+
+        strcat(buffer, argv[i]);  // add current argument
+
+        // add '-' if it's not the last one
+        if (i < argc - 1) {
+            strcat(buffer, "-");
+        }
+    }
 
     printf("%s\n",buffer); // use only this single print statement in your submitted code
 
